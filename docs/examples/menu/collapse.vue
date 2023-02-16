f9f197882077c389c3e2cf730c0b4cc518b4d2d6
Xrc1314502 <39941245+Xrc1314502@users.noreply.github.com>
2023-02-16 18:11:03 +0800

    Update collapse.vue

--- docs/examples/menu/collapse.vue
+++ docs/examples/menu/collapse.vue
@@ -10,6 +10,19 @@
     @open="handleOpen"
     @close="handleClose"
   >
+    
+    <el-menu-item index="2">
+      <el-icon><icon-menu /></el-icon>
+      <template #title>Navigator Two</template>
+    </el-menu-item>
+    <el-menu-item index="3" disabled>
+      <el-icon><document /></el-icon>
+      <template #title>Navigator Three</template>
+    </el-menu-item>
+    <el-menu-item index="4">
+      <el-icon><setting /></el-icon>
+      <template #title>Navigator Four</template>
+    </el-menu-item>
     <el-sub-menu index="1">
       <template #title>
         <el-icon><location /></el-icon>
@@ -28,18 +41,6 @@
         <el-menu-item index="1-4-1">item one</el-menu-item>
       </el-sub-menu>
     </el-sub-menu>
-    <el-menu-item index="2">
-      <el-icon><icon-menu /></el-icon>
-      <template #title>Navigator Two</template>
-    </el-menu-item>
-    <el-menu-item index="3" disabled>
-      <el-icon><document /></el-icon>
-      <template #title>Navigator Three</template>
-    </el-menu-item>
-    <el-menu-item index="4">
-      <el-icon><setting /></el-icon>
-      <template #title>Navigator Four</template>
-    </el-menu-item>
   </el-menu>
 </template>
 
